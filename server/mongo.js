31836eec6b7d62b6264c00fad448bbe2fda9c55e
avidraccoon <112419854+avidraccoon@users.noreply.github.com>
2024-06-18 18:18:44 +0000

    Add security

--- server/mongo.js
+++ server/mongo.js
@@ -1,5 +1,9 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://nathansmith:AvidGameDev@cluster0.ujlon0j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env'});
+const username = process.env.DATABASE_USERNAME;
+const password = process.env.DATABASE_PASSWORD;
+const uri = "mongodb+srv://"+username+":"+password+"@cluster0.ujlon0j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 let client = new MongoClient(uri, {
